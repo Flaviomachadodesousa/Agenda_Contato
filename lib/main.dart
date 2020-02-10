d641cec131f30f707abaf5ddbf730a2972cd4a61
Flavio <flavio.sos.machado@gmail.com>
2020-02-10 16:02:07 -0300

    se

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:agenda_contato/ui/contact.page.dart';
 import 'package:flutter/material.dart';
+import 'package:agenda_contato/ui/home.page.dart';
 
 void main(){
-  runApp(MaterialApp)
-};
+  runApp(MaterialApp(
+  home: ContactPage(),
+  debugShowCheckedModeBanner: false,
+  ));
+}
