c2f0e27f44a7fa15c39972306e1a927ed7c8ea9d
Flavio <flavio.sos.machado@gmail.com>
2020-02-12 10:53:35 -0300

    fnal

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:agenda_contato/ui/contact.page.dart';
+import 'package:agenda_contato/ui/home.page.dart';
 import 'package:flutter/material.dart';
 
 
 void main(){
   runApp(MaterialApp(
-  home: ContactPage(),
+  home: HomePage(),
   debugShowCheckedModeBanner: false,
   ));
 }
